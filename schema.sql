6d3fc2efc916b750b7734ee042d129652c0c2ca0
Gopi Gongalareddy <siribasireddi@gmail.com>
2019-10-23 21:17:52 -0400

    Modified SChema added

--- schema.sql
+++ schema.sql
@@ -1,21 +1,16 @@
-CREATE DATABASE backendproject
-\c backendproject
-
 
 CREATE TABLE company (
    id SERIAL PRIMARY KEY,
-   cid VARCHAR(6) PRIMARY KEY, --defined on new client acquisition
+   cid VARCHAR(6), 
    name TEXT NOT NULL,
-   location VARCHAR(50),
+   location VARCHAR(50)
 );
-
-
 
 CREATE TABLE employee (
     id SERIAL PRIMARY KEY,
-    eid VARCHAR(4) NOT NULL, --this is co specified
-    co_cid VARCHAR(6) REFERENCES company(id) on DELETE CASCADE, --
-    t_id INT REFERENCES time_punch (id), 
+    eid VARCHAR NOT NULL, 
+    co_cid INTEGER REFERENCES company(id) on DELETE CASCADE, 
+    t_id INTEGER REFERENCES time_punch(id), 
     firstname TEXT NOT NULL,
     lastname TEXT NOT NULL,
     password VARCHAR(20)
@@ -23,7 +18,7 @@ CREATE TABLE employee (
 
 
 CREATE TABLE time_punch (
-    id SERIAL PRIMARY KEY REFERENCES employee(eid),
+    id SERIAL PRIMARY KEY,
     starttime timestamp without time zone,
     endtime timestamp without time zone,
     worktype text,
