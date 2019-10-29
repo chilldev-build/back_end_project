726b74fc33c3434be35166357f35af779d89c0fb
Chris Hill <chrishillbuilds@gmail.com>
2019-10-29 13:12:55 -0400

    added team stuff

--- models/team_model.js
+++ models/team_model.js
@@ -1,114 +1,87 @@
 const db = require('./conn');
-
 class Team{
-    constructor(id,team_id,employee,lastworkedon,activity,totalhoursperweek,week){
-        this.id = id;
-        this.team_id = team_id;
-        this.employee = employee;
-        this.lastworkedon = lastworkedon;
-        this.activity = activity;
-        this.totalhoursperweek = totalhoursperweek;
-        this.week = week;
-    }
-    static async getAllweek(){
-
-        try{
-            const response = await db.one(`SELECT week
-                                        FROM time_punch where id = 58;
-                                        ;`
-                            );
-            console.log("Week data :", response);
-            return response.week;
-
-        }catch(error){
-            return error.message;
-        }
-
-    }
-
-    static async checkactivity(){
-        let week =  await this.getAllweek();
-        console.log("week", week);
-        let activity = null
-        if(week>0 && week <= 10) {
-            activity = 25+'%';
-            console.log("activity", activity);
-            return activity;
-        }
-        if(week>10 && week <= 20) {
-            activity = 50+'%';
-            console.log("activity", activity);
-            return activity;
-        }
-        if(week>20 && week <= 30) {
-            activity = 75+'%';
-            console.log("activity", activity);
-                return activity;
-        }
-        if(week>30 && week <40) {
-            activity = 90+'%';
-            console.log("activity", activity);
-            return activity;
-        }
-        else{
-            return activity;
-        }
+   constructor(id,team_id,employee,lastworkedon,activity,totalhoursperweek,week){
+       this.id = id;
+       this.team_id = team_id;
+       this.employee = employee;
+       this.lastworkedon = lastworkedon;
+       this.activity = activity;
+       this.totalhoursperweek = totalhoursperweek;
+       this.week = week;
+   }
+   static async getAllweek(){
+       try{
+           const response = await db.one(`SELECT week
+                                       FROM time_punch where id = 58;
+                                       ;`
+                           );
+           console.log("Week data :", response);
+           return response.week;
+       }catch(error){
+           return error.message;
+       }
+   }
+   static async checkactivity(){
+       let week =  await this.getAllweek();
+       console.log("week", week);
+       let activity = null
+       if(week>0 && week <= 10) {
+           activity = 25+'%';
+           console.log("activity", activity);
+           return activity;
+       }
+       if(week>10 && week <= 20) {
+           activity = 50+'%';
+           console.log("activity", activity);
+           return activity;
+       }
+       if(week>20 && week <= 30) {
+           activity = 75+'%';
+           console.log("activity", activity);
+               return activity;
+       }
+       if(week>30 && week <40) {
+           activity = 90+'%';
+           console.log("activity", activity);
+           return activity;
+       }
+       else{
+           return activity;
+       }
 }
-
-    static async totalhours(){
-        let week = await this.getAllweek();
-        console.log("week", week);
-        try{
-            const response = await db.one(
-                `UPDATE team SET totalhoursperweek = (week,"<iframe src ='./views/template' 
-                width = "100" height ="100"></iframe>");`
-            );
-            console.log("Total hours ", response);
-            return response;
-        }catch(err){
-            return err.message;
-        }
-
-
-    }
-
-     static async getAllteamdata(){
-
-        try{
-            const response = await db.any(`SELECT team.employee,
-                                        team.lastworkedon,
-                                        team.activity,
-                                        team.totalhoursperweek
-                                        FROM team INNER JOIN time_punch
-                                        on team.id = time_punch.id
-                                        ORDER BY team.id;`
-                            );
-            console.log("team data :", response);
-            return response;
-
-        }catch(error){
-            return error.message;
-        }
-    }
-
-     static async save(activity){
-        console.log("activity:",activity);
-        try{
-            const response = await db.one(
-                `INSERT INTO team(activity) VALUES ($1) RETURNING id;`,
-                    [   
-                        activity
-                    ]
-            );
-           
-            return response;
-        }catch(err){
-            return err.message;
-        }
-       
-    }
+   static async save(){
+       console.log("inside sAVE");
+       const activity = await this.checkactivity();
+       console.log("activity sirisha:",activity);
+       let index = 0;
+       let response;
+       while (index < activity.length) {
+           try{
+               //console.log(Saving employe ID: ${index + 1} with activity of ${activity[index]});
+               response = await db.result(`UPDATE team SET activity = $1 WHERE id = $2;`, [activity[index], index + 1]);
+               console.log("activity response11", response);
+           }catch(error){
+               return error.message;
+           }
+           index++;
+       }
+           return response;
+   }
+    static async getAllteamdata(){
+       try{
+           const response = await db.any(`SELECT team.employee,
+                                       team.lastworkedon,
+                                       team.activity,
+                                       team.totalhoursperweek
+                                       FROM team INNER JOIN time_punch
+                                       on team.id = time_punch.id
+                                       ORDER BY team.id;`
+                           );
+           console.log("team data :", response);
+           return response;
+       }catch(error){
+           return error.message;
+       }
+   }
 }
-
 module.exports = Team;
-
-
--- public/javascripts/barchart.js
+++ public/javascripts/barchart.js
@@ -1,45 +1,39 @@
-var ctx = document.getElementById("myChart");
-var myChart = new Chart(ctx, {
-  type: 'bar',
-  data: {
-    labels: ["Mon", "Tue", "Wed", "Thu", "Fri",],
-    datasets: [{
-      label: '# of Days',
-      data: [22, 19, 40, 5, 2, 3, 20, 3],
-      backgroundColor: [
-        'rgba(0, 99, 132, 0.6)',
-        'rgba(0, 99, 132, 0.6)',
-        'rgba(0, 99, 132, 0.6)',
-        'rgba(0, 99, 132, 0.6)',
-        'rgba(0, 99, 132, 0.6)',
-        'rgba(0, 99, 132, 0.6)'
-        
-      ],
-      borderColor: [
-        'rgba(0, 99, 132, 0.6)',
-        'rgba(0, 99, 132, 0.6)',
-        'rgba(0, 99, 132, 0.6)',
-        'rgba(0, 99, 132, 0.6)',
-        'rgba(0, 99, 132, 0.6)'
-        
-      ],
-      borderWidth: 1
-    }]
-  },
-  options: {
-    responsive: false,
-    scales: {
-      xAxes: [{
-        ticks: {
-          maxRotation: 90,
-          minRotation: 80
-        }
-      }],
-      yAxes: [{
-        ticks: {
-          beginAtZero: true
-        }
-      }]
-    }
-  }
+var employeeCanvas = document.getElementById("myChart");
+Chart.defaults.global.defaultFontFamily = "Lato";
+Chart.defaults.global.defaultFontSize = 18;
+var weekData = {
+ label: 'week',
+ data: [19,28,9,35,40],
+ backgroundColor: 'rgba(0, 99, 132, 0.6)',
+ borderWidth: 0,
+ yAxisID: "y-axis-density"
+};
+var hoursData = {
+ label: 'hours',
+ data: [8.9, 9.8, 3.7, 23.1, 9.0, 8.7, 11.0],
+ backgroundColor: 'rgba(99, 132, 0, 0.6)',
+ borderWidth: 0,
+ yAxisID: "y-axis-gravity"
+};
+var employeeData = {
+ labels: ["Mon", "Tue", "Wed", "Thu", "Fri"],
+ datasets: [weekData, hoursData]
+};
+var chartOptions = {
+ scales: {
+   xAxes: [{
+     barPercentage: 1,
+     categoryPercentage: 0.6
+   }],
+   yAxes: [{
+     id: "y-axis-density"
+   }, {
+     id: "y-axis-gravity"
+   }]
+ }
+};
+var barChart = new Chart(employeeCanvas, {
+ type: 'bar',
+ data: employeeData,
+ options: chartOptions
 });
